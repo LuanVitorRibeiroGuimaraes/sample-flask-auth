22d70201f7a8aa1dca8f5956a0b3862eb806202e
Lv777N <luanvitor.mat@gmail.com>
2025-04-04 01:38:31 -0300

    Create routes logout and user for add user

--- app.py
+++ app.py
@@ -1,33 +1,22 @@
 from flask import Flask, request, jsonify
 from models.User import User
 from login import login_manager
-from flask_login import login_user, current_user
+from flask_login import login_user, current_user, logout_user, login_required
 from database import db
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'my_secret_key' #chave secreta
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///project.db' #URL de conexão
 
-#session <- armazena a conexão ativa, possui tempo limite
 db.init_app(app)
-login_manager.init_app(app) #Vincula o Flask-Login ao app.
-
-#view de login
-login_manager.login_view = 'login' #Define a página para onde usuários não logados serão redirecionados.
+login_manager.init_app(app)
 
 
-# Antes de acessar uma rota protegida (@login_required)
-# Se um usuário tentar acessar uma rota protegida, o Flask precisa verificar:
-# O usuário está autenticado?
-# Ele tem permissão para acessar essa página?
-# Para isso, recupera o usuário usando o user_loader.Antes de acessar uma rota protegida (@login_required)
-# Se um usuário tentar acessar uma rota protegida, o Flask precisa verificar:
-# O usuário está autenticado?
-# Ele tem permissão para acessar essa página?
-# Para isso, recupera o usuário usando o user_loader.
+login_manager.login_view = 'login' 
 
-@login_manager.user_loader #Diz ao Flask-Login como carregar o usuário a partir do ID salvo na sessão.
-def load_user(user_id): #Busca e retorna o usuário correspondente ao ID. verifica permissões, acessa atributos e verifica se está autenticado
+
+@login_manager.user_loader
+def load_user(user_id):
     return User.query.get(user_id)
 
 @app.route('/login', methods = ['POST'])
@@ -41,12 +30,31 @@ def login():
         user = User.query.filter_by(username = username).first()
         
         if user and user.password == password:
-            login_user(user) #loga o usuário (salva o id na sessão)
-            # print(current_user.is_authenticated)
+            login_user(user)
             return jsonify({'message':'Successfully auth'})
         
     return jsonify({'message':'Invalid Credential'}), 400
 
+@app.route('/logout', methods = ['GET'])
+@login_required
+def logout():
+    logout_user()
+    return jsonify({'message':'Logout successfully'})
+
+@app.route('/user', methods = ['POST'])
+@login_required
+def create_user():
+    data = request.get_json()
+    username = data.get('username')
+    password = data.get('password')
+    
+    if username and password:
+        user = User(username = username, password = password)
+        db.session.add(user)
+        db.session.commit()
+        return jsonify({'message':'User registred succesfully!'})
+    
+    return jsonify({'message':'Invalid credentials'}), 400
 
 @app.route('/hello-world', methods = ['GET'])
 def hello_world():
--- models/User.py
+++ models/User.py
@@ -1,6 +1,7 @@
 from database import db
 from flask_login import UserMixin
 
+#db.Model gera um construtor automático
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key = True)
     username = db.Column(db.String(80), unique_key = True, nullable = False)
